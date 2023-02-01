dcaead4e5c8a2555cb3965d67af5017848ac4bc1
Alexander <alexanderpastfast@gmail.com>
2023-02-01 11:06:04 +0300

    first comm

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="#" alt="" />
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  );
+};
+export default Profile;
